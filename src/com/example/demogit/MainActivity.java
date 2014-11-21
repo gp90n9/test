06ce7475cec7bf4206a3cc586ccb68d9d81581e5
gp90n9 <82654060@qq.com>
2014-11-21 23:32:53 +0800

    jj

--- src/com/example/demogit/MainActivity.java
+++ src/com/example/demogit/MainActivity.java
@@ -7,9 +7,11 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// sss
+	}
 }
